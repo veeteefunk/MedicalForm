ff78511c91e6ede200680d3257c706fe79e6bb12
veeteefunk <victormarinviloria@gmail.com>
2019-07-14 15:07:48 +0200

    Updates

---
+++ precache-manifest.f58965eff48696ba9fa0abe23df61aaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5772215dfcf5ee0a1c53e739052150",
+    "url": "/MedicalForm/index.html"
+  },
+  {
+    "revision": "ddf025c712241da65b93",
+    "url": "/MedicalForm/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "02732569fe1d230c8444",
+    "url": "/MedicalForm/static/js/2.d86fa02c.chunk.js"
+  },
+  {
+    "revision": "ddf025c712241da65b93",
+    "url": "/MedicalForm/static/js/main.10e7b0bc.chunk.js"
+  },
+  {
+    "revision": "ea689808168ce2fd5c30",
+    "url": "/MedicalForm/static/js/runtime~main.dd53519d.js"
+  }
+]);
